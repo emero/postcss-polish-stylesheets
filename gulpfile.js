5c90b5bcc42132cb6f60108aa4beb3f053718d34
Michał Mrozowski <michal.mrozowski@10clouds.com>
2016-01-11 22:45:36 +0100

    Bumped up posts version, added some color names

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
 gulp.task('test', function () {
   return gulp.src('test/test.js', { read: false })
-    .pipe(mocha());
+    .pipe(mocha())
+    .on("error", handleError);
 });
 
 gulp.task('default', ['test']);
